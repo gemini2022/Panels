dff9b860069235748fc5a1e1352aece8d89d20b5
Bron LaPoint <gemini2022@yahoo.com>
2024-05-20 15:10:10 -0400

    Created panel library

---
+++ projects/panel/src/lib/panel.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-panel',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      panel works!
+    </p>
+  `,
+  styles: ``
+})
+export class PanelComponent {
+
+}
---
+++ projects/panel/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of panel
+ */
+
+export * from './lib/panel.service';
+export * from './lib/panel.component';
